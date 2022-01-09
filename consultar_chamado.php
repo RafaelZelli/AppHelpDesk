2775b2408bbf7fcec5c50c9541d02b4ed1f6191f
Rafael Zelli <rafazelli@gmail.com>
2022-01-09 13:55:18 -0300

    Consultando chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,5 +1,22 @@
 <?php require_once "validador_acesso.php";  ?>
 
+<?php
+    // array com os chamados
+    $chamados = array();
+
+    // Abre o arquivo.hd
+    $arquivo = fopen('arquivo.hd', 'r');
+
+    // Enquanto houver registros(linhas) a serem recuperado
+    while(!feof($arquivo)){ // testa pelo fim do arquivo
+      $registro = fgets($arquivo);  //linhas
+      $chamados[] = $registro;
+    }
+
+    // Fecha o arquivo aberto
+    fclose($arquivo);
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -40,25 +57,27 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
 
-                </div>
-              </div>
+              <?php foreach($chamados as $chamado){ ?>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <?php
+                $chamado_dados = explode('#', $chamado);
 
-                </div>
-              </div>
+                if(count($chamado_dados) < 3){
+                  continue;
+                }
+              ?>
 
+                  <div class="card mb-3 bg-light">
+                    <div class="card-body">
+                     <h5 class="card-title"><?= $chamado_dados[0]?></h5>
+                      <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]?></h6>
+                      <p class="card-text"><?= $chamado_dados[2]?></p>
+                    </div>
+                  </div>
+
+                <?php } ?> 
+ 
               <div class="row mt-5">
                 <div class="col-6">
                   <a class="btn btn-lg btn-warning btn-block" href="home.php" >Voltar</a>
